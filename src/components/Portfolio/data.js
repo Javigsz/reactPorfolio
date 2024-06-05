043bcce9324dc25197cc0a072b78773fa5e369a0
Javigsz <javiguerrerosanchez@gmail.com>
2024-06-05 21:11:49 +0200

    initial commit

---
+++ src/components/Portfolio/data.js
@@ -0,0 +1,60 @@
+const projects = [
+    {
+        id: 1,
+        name: 'Tramazroc',
+        desc: 'Python application for automatic detection of rock mass degradation.',
+        link: 'https://github.com/Javigsz/TraMazRoc',
+        img: '/tramazroc.png',
+        tools: [
+            'python, ',
+            'numpy, ',
+            'pandas, ',
+            'tkinter, ',
+            'opencv'
+        ]
+    }, 
+    {
+        id: 2,
+        name: 'GameSearch',
+        desc: 'Web application for searching info about any game that existed',
+        link: 'https://github.com/Javigsz/react/projects/gamesearch',
+        img: '/gamesearch2.png',
+        tools: [
+            'html, ',
+            'javascript, ',
+            'css, ',
+            'react, ',
+        ]
+    }, 
+    {
+        id: 3,
+        name: 'Django Blog',
+        desc: 'Web blog made with Django backend framework',
+        link: 'https://github.com/Javigsz/miweb',
+        img: '/django.png',
+        tools: [
+            'html, ',
+            'javascript, ',
+            'css, ',
+            'python, ',
+            'django, ',
+            'bootstrap'
+        ]
+    }, 
+    {
+        id: 4,
+        name: 'React repo',
+        desc: 'GitHub repo with all self made projects',
+        link: 'https://github.com/Javigsz/react',
+        img: '/react.png',
+        tools: [
+            'html, ',
+            'javascript, ',
+            'css, ',
+            'react, ',
+            'nodejs'
+        ]
+    }, 
+]
+
+export default projects
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,48 @@
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import projects from './data'
+import './index.scss'
+
+const Portfolio = () => {
+
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            Portfolio
+          </h1>
+          <form action="https://github.com/Javigsz">
+            <input type="submit" value="Github" className="flat-button" />
+          </form>
+        </div>
+        <div className="todo">
+          {projects.map(project => 
+            <div className="caja" key={project.id} onClick={() => window.open(project.link, '_blank')}>
+              <img src={project.img} alt={project.name} />
+              <div className="info">
+                <h3><b>{project.name}</b></h3>
+                <p>{project.desc}</p>
+              </div>
+              Tools:&nbsp; 
+              {project.tools.map(tool => 
+                <span>{tool}</span>
+              )}
+
+            </div>
+          )}
+          <div className="caja"></div>
+        </div>
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Portfolio
+
+/*           {project.tools.map(tool => 
+  <span>{tool}</span>
+)}*/
